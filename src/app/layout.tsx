46ab028e441c1a2bf62c57ce4fc1ac4206e75886
Paul Bayfield <137620008+PaulBayfield@users.noreply.github.com>
2026-05-14 11:01:34 +0200

    feat: add umami analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Damion } from "next/font/google";
 import type { Metadata } from "next";
+import UmamiProvider from "next-umami";
 
 export const metadata: Metadata = {
   title: "Vincent Van Gogh",
@@ -28,6 +29,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
+      <head>
+        <UmamiProvider
+          websiteId="a9057ce9-86ca-44e1-a5fe-a1923b7e5024"
+          src="https://analytics.bayfield.dev/script.js"
+        />
+      </head>
       <body className={damion.className}>
         {children}
       </body>
